a052c2a2544fc2b87c53fa57d79a934d16c3e6d3
Dylan Hall <fdhall22@gmail.com>
2017-04-20 22:23:06 -0400

    Added beginnings of spell lists, other aesthetic code changes

--- Character-Randomizer.py
+++ Character-Randomizer.py
@@ -18,7 +18,8 @@ class Character(object):
         self.characterclass = self.char_class()
         self.characterrace = self.char_race()
         self.stats = self.char_stats()
-        self.traits = self.char_traits(self.characterrace)
+        self.traits = self.char_traits()
+        self.spells = self.char_spells()
         self.character_info()
 
     def char_class(self):
@@ -38,8 +39,8 @@ class Character(object):
         return stats
 
     """this is a long list of traits for each race which will be said in the output of the character"""
-    def char_traits(self,race):
-        self.race = race
+    def char_traits(self):
+        race = self.characterrace
         #Dwarf
         if race == 'Hill Dwarf' or race == 'Mountain Dwarf':
             if race == 'Hill Dwarf':
@@ -93,13 +94,67 @@ class Character(object):
             speed = "Your base walking speed is 30 feet."
             language = "You can speak, read and write Common and One extra language of your choosing. "
             special = ""
+
             return absc, age, size, speed, language, special
         else:
             return "What?"
 
-    def char_spells(self, charclass):
-        #may do this at some point
-        exit(1)
+    def char_spells(self):
+        if self.characterclass == 'Cleric' or self.characterclass == 'Wizard':
+            num0 = 3
+            num1 = 2
+        else:
+            break
+        cantrip_spells = []
+        level_1_spells = []
+        cantrips = ['Acid Splash',
+        'Dancing Lights',
+        'Fire Bolt',
+        'Guidance',
+        'Light',
+        'Mage Hand',
+        'Minor Illusion',
+        'Poison Spray',
+        'Prestidigitation',
+        'Ray Of Frost',
+        'Resistance',
+        'Sacred Flame',
+        'Shocking Grasp',
+        'Spare the Dying',
+        'Thaumaturgy']
+
+        level_1 = ['Bless',
+        'Burning Hands',
+        'Charm Person',
+        'command',
+        'Comprehend Languages',
+        'Cure Wounds',
+        'Detect Magic',
+        'Disguise self',
+        'Faerie fire',
+        'Guiding Bolt',
+        'Healing Word',
+        'Identify',
+        'Inflict Wounds',
+        'Mage Armor',
+        'Magic Missile',
+        'Sanctuary',
+        'Shield',
+        'Shield of Faith',
+        'Silent Image',
+        'Sleep',
+        'Thunderwave']
+        for x in range(num0):
+            lvl0 = cantrips[randint(0,len(cantrips)-1)]
+            cantrip_spells.append(lvl0)
+            cantrips.remove(lvl0)
+
+        for y in range(num1):
+            lvl1 = level_1[randint(0,len(level_1)-1)]
+            level_1_spells.append(lvl1)
+            level_1.remove(lvl1)
+
+        return cantrip_spells, level_1_spells
 
     def char_items(self, charclass):
         #may do this at some point
@@ -108,20 +163,19 @@ class Character(object):
     def character_info(self):
         #all my newlines and tabs are because I like the command line more neat, in reality it doesnt matter.
         stat_name = ['Strength','Wisdom','Intelligence','Constitution','Dexterity','Charisma']
-        player_stats = self.stats
         FINAL_STATS = []
-        print "\n\tYour race is {} and your class is {}\n".format(self.characterrace, self.characterclass)
-        print "Please choose one of the following for your stat: {}".format(player_stats)
-        for stat_num in range(len(player_stats)):
+        print "\n\tYour race is {} and your class is {}\n".format(self.characterrace,self.characterclass )
+        print "Please choose one of the following for your stat: {}".format(self.stats)
+        for stat_num in range(len(self.stats)):
             while True:
                 user = raw_input("Please input one of these stats for your " + stat_name[stat_num] + ": ")
-                if user.isdigit(): #cant use isnumeric because of unicode?
+                if user.isdigit():
                     stat_choice = int(user)
-                    if stat_choice in player_stats:
+                    if stat_choice in self.stats:
                         FINAL_STATS.append(str(stat_name[stat_num]) + ": " + str(stat_choice) )
-                        player_stats.remove(stat_choice)
+                        self.stats.remove(stat_choice)
                         break
-            #player_stats[stat_num] = stat_choice
+
         print "\n\tSUMMARY"
         print "\n\tYour class is {} and your race is {}\n".format(self.characterrace,self.characterclass)
         print '''
@@ -131,10 +185,14 @@ class Character(object):
     Size: {}
     Speed: {}
     Age: {}
-    Other: {}
+    {}
      '''.format(self.traits[0], self.traits[1], self.traits[2], self.traits[3], self.traits[4], self.traits[5])
 
-        print "\tYour final stats are as follows:\n{} {} {} {} {} {}".format(FINAL_STATS[0],FINAL_STATS[1],FINAL_STATS[2],FINAL_STATS[3],FINAL_STATS[4],FINAL_STATS[5])
+        print "\t\t\tYour final stats are as follows:\n\t{} {} {} {} {} {}".format(FINAL_STATS[0],FINAL_STATS[1],FINAL_STATS[2],FINAL_STATS[3],FINAL_STATS[4],FINAL_STATS[5])
 
 #character testing
-char1 = Character()
+if __name__ == "__main__":
+    print "Welcome to my Dungeons & Dragons character randomizer"
+    first_character = Character()
+    while raw_input("\nWould you like to create a character?(no to Quit)\n") !=  'no':
+        random_character = Character()
